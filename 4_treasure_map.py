fac79e6162c71d41557bb5bbd80321b516607e16
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-11 17:58:54 +0200

    Create 4_treasure_map.py
    practice with list

---
+++ 4_treasure_map.py
@@ -0,0 +1,12 @@
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+column = int(position[0])
+row = int(position[1])
+
+map[row-1][column-1] = "👑"
+print(f"{row1}\n{row2}\n{row3}")

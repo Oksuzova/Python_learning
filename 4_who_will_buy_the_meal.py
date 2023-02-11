e71b446d3337d154582848cfa9c8132c74d7c872
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-11 16:41:02 +0200

    Update 4_who_will_buy_the_meal.py

--- 4_who_will_buy_the_meal.py
+++ 4_who_will_buy_the_meal.py
@@ -7,3 +7,7 @@ index = len(names) - 1
 ch = random.randint(0, index)
 
 print(f"{names[ch]} is going to buy the meal today!")
+
+# alternative solution
+# ch = random.choice(names)
+# print(ch + " is going to buy the meal today!")

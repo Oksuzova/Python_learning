9d2acaa1efc4f31e7fea27e6539b123db7e6cdf8
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-11 16:32:55 +0200

    Create 4_who_will_buy_the_meal.py
    Practice with random function

---
+++ 4_who_will_buy_the_meal.py
@@ -0,0 +1,9 @@
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+index = len(names) - 1
+ch = random.randint(0, index)
+
+print(f"{names[ch]} is going to buy the meal today!")

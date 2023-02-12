79c2d853bfc5de5d66e1683757fc617c59f4735a
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-12 02:02:18 +0200

    Update 5_FizzBuzz_game.py

--- day005/5_FizzBuzz_game.py
+++ day005/5_FizzBuzz_game.py
@@ -1,16 +1,26 @@
-nums = list()
+# nums = list()
+#
+# for i in range(1, 101):
+#     nums.append(i)
+#
+# for i in nums:
+#     if i % 3 == 0 and i % 15 != 0:
+#         print("Fizz")
+#         continue
+#     elif i % 5 == 0 and i % 15 != 0:
+#         print("Buzz")
+#         continue
+#     elif i % 15 == 0:
+#         print("FizzBuzz")
+#     else:
+#         print(nums[i-1])
 
-for i in range(1, 101):
-    nums.append(i)
-
-for i in nums:
-    if i % 3 == 0 and i % 15 != 0:
-        print("Fizz")
-        continue
-    elif i % 5 == 0 and i % 15 != 0:
-        print("Buzz")
-        continue
-    elif i % 15 == 0:
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
     else:
-        print(nums[i-1])
+        print(num)

c9df2ab344cfcaf477c2bba2c4c2161eecc0e34d
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-14 10:42:08 +0200

    function. simple constructions

---
+++ day008/7_simple function.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print(f"Hello {name}")
+    print("Nice to meet you")
+
+greet("Tanya")
---
+++ day008/8_area_calculator.py
@@ -0,0 +1,10 @@
+import math
+def paint_calc(height, width, cover):
+    cans = math.ceil(height * width / coverage)
+    print(f"You'll need {cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+

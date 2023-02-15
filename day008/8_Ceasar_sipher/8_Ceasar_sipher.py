c9a91a4376aaa327a4019be489f106c4df544fba
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-15 19:24:37 +0200

    Update 8_Ceasar_sipher.py

--- day008/8_Ceasar_sipher/8_Ceasar_sipher.py
+++ day008/8_Ceasar_sipher/8_Ceasar_sipher.py
@@ -1,15 +1,15 @@
 import string
-import time
+
+
 def encrypt(text, shift, direction):
-    start = time.time()
     entext = list()
     if direction == "decode":
         shift *= -1
     for letter in text:
-        if letter in alphabet:
-            position = alphabet.index(letter)
+        if letter in string.ascii_lowercase:
+            position = string.ascii_lowercase.index(letter)
             new_position = (position + shift) % 26
-            new_letter = alphabet[new_position]
+            new_letter = string.ascii_lowercase[new_position]
         else:
             new_letter = letter
         entext.append(new_letter)
@@ -17,20 +17,15 @@ def encrypt(text, shift, direction):
     result = open('text.txt', 'w')
     result.write(entext)
     result.close()
-    end = time.time()
-    print(end)
 
-
-alphabet = list(string.ascii_lowercase)                              #add the list of alphabet letters
 
 choice = False
 while not choice:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    doc = open("text.txt")
-    text = list()
-    for line in doc:
-        text.append(line)
-    text = "".join(text)
+
+    text = open("text.txt", "r")
+    text = text.read()
+
     shift = int(input("Type the shift number:\n"))
 
     encrypt(text, shift, direction)

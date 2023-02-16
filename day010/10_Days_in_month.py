8dd7076e8e3259c95701d270379bf6fcaa49368f
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-16 19:24:06 +0200

    Update 10_Days_in_month.py

--- day010/10_Days_in_month.py
+++ day010/10_Days_in_month.py
@@ -13,14 +13,9 @@ def is_leap(year):
 
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    for q in month_days:
-        if month != 2:
-            return month_days[month - 1]
-        else:
-            if not is_leap(year):
-                return month_days[month - 1]
-            else:
-                return (month_days[month - 1] + 1)
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month -1]
 
 
 year = int(input("Enter a year: "))

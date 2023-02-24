49b802e0bebfd52d55c7dc58733160ddddcff649
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-02-24 20:44:15 +0200

    Coffee Machine OOP version

--- day016/16.py
+++ day016/16.py
@@ -1,11 +1,22 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+
+my_table = PrettyTable()
+
+my_table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+my_table.add_column("Type", ["Electric", "Water", "Fire"])
+my_table.align = "l"
+
+print(my_table)
---
+++ day016/oop-coffee-machine-start/main.py
@@ -0,0 +1,29 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+make_coffee = CoffeeMaker()
+money = MoneyMachine()
+
+on = True
+
+while on:
+    users_input = input(f"What would you like? ({menu.get_items()})?: ")
+    if users_input == "off":
+        on = False
+        break
+    elif users_input == "report":
+        make_coffee.report()
+        money.report()
+    elif users_input in menu.get_items():
+        order = menu.find_drink(users_input)
+        if make_coffee.is_resource_sufficient(order):
+            try:
+                if money.make_payment(order.cost):
+                    make_coffee.make_coffee(order)
+            except:
+                print("Invalid data. Try again")
+    else:
+        print("Invalid data. Try again")
+

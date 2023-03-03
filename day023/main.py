9ecbbcbbafb493389e3542d83177f3d825431962
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-03-04 00:08:08 +0200

    create turtle crossing game

---
+++ day023/main.py
@@ -0,0 +1,25 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+
+player = Player()
+
+screen.onkey(player.go_up, key="Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    if player.ycor() == 280:
+        player.next_level()
+
+
+screen.exitonclick()

b945fa1050b33c97c177fe9141da5d520d889be1
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-03-04 12:46:30 +0200

    .

--- day023/car_manager.py
+++ day023/car_manager.py
@@ -18,10 +18,10 @@ class CarManager(Turtle):
         new_car.turtlesize(1, 2)
         new_car.color(random.choice(COLORS))
         new_car.penup()
-        new_car.goto(random.randint(300, 350), random.randint(-270, 270))
+        new_car.goto(random.randint(300, 350), random.randint(-250, 250))
         self.cars.append(new_car)
 
     def move(self):
-        for _, cars in enumerate(self.cars):
-            new_x = self.cars[_].xcor() - STARTING_MOVE_DISTANCE
-            self.cars[_].goto(new_x, cars.ycor())
+        for car in self.cars:
+            new_x = car.xcor() - STARTING_MOVE_DISTANCE
+            car.goto(new_x, car.ycor())
--- day023/main.py
+++ day023/main.py
@@ -15,6 +15,8 @@ car = CarManager()
 screen.onkey(player.go_up, key="Up")
 
 game_is_on = True
+gen = 0
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
@@ -22,8 +24,11 @@ while game_is_on:
     if player.ycor() == 280:
         player.next_level()
 
-    car.move()
-    car.new_car()
+    if gen == 6:
+        car.new_car()
+        gen = 0
 
+    car.move()
+    gen += 1
 
 screen.exitonclick()

25d30150c6a1646b36d72d0165c168a4c1bfb8b3
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-03-10 00:11:26 +0200

    Update List_Comprehension.py

--- day026/List_Comprehension.py
+++ day026/List_Comprehension.py
@@ -1,5 +1,7 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
 squared_numbers = [x*x for x in numbers]
-
 print(squared_numbers)
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+result = [nums for nums in numbers if nums % 2 == 0]
+print(result)

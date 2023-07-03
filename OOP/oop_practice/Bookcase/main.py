8a6047d074f1230930b97d1af14def49567aecbb
Oksuzova <42168285+Oksuzova@users.noreply.github.com>
2023-07-03 23:32:21 +0300

    .

--- OOP/oop_practice/Bookcase/main.py
+++ OOP/oop_practice/Bookcase/main.py
@@ -2,53 +2,49 @@ from book_manager import Closet
 from bookshelf import Shelf
 from book import Book
 
-# read_book = ReadBooks()
-# unread_book = UnReadBooks()
-# bookshelf = Bookshelves()
 
 book1 = Book(name="Harry Potter", author="J.K. Rolling")
 book2 = Book(name="Pride and Prejudice", author="Jane Austen")
 book3 = Book(name="1984", author="George Orwell")
-
 
 closet = Closet()
 
 closet.take(book1.name, book1.author)
 closet.take(book2.name, book2.author)
 
-# print(f"Count unread books: {closet.count_unread}")
-# print(f"Count read books: {closet.count_read}")
-# print(f"Total count books: {closet.total}")
-#
-# print("Read book1\n...")
-# closet.reading(book1.name)
-#
-# print(f"Count unread books: {closet.count_unread}")
-# print(f"Count read books: {closet.count_read}")
-# print(f"Total count books: {closet.total}")
-#
-# print("Read book2\n...")
-# closet.reading(book2.name)
-#
-# print(f"Count unread books: {closet.count_unread}")
-# print(f"Count read books: {closet.count_read}")
-# print(f"Total count books: {closet.total}")
-#
-# print("Take book3\n...")
-# closet.take(book3.name, book3.author)
-#
-# print(f"Count unread books: {closet.count_unread}")
-# print(f"Count read books: {closet.count_read}")
-# print(f"Total count books: {closet.total}")
-#
-# print("Retrieve book1\n...")
-# closet.retrieve(book1.name)
-#
-# print(f"Count unread books: {closet.count_unread}")
-# print(f"Count read books: {closet.count_read}")
-# print(f"Total count books: {closet.total}")
-#
-# print(closet)
+print(f"Count unread books: {closet.count_unread}")
+print(f"Count read books: {closet.count_read}")
+print(f"Total count books: {closet.total}")
+
+print("Read book1\n...")
+closet.reading(book1.name)
+
+print(f"Count unread books: {closet.count_unread}")
+print(f"Count read books: {closet.count_read}")
+print(f"Total count books: {closet.total}")
+
+print("Read book2\n...")
+closet.reading(book2.name)
+
+print(f"Count unread books: {closet.count_unread}")
+print(f"Count read books: {closet.count_read}")
+print(f"Total count books: {closet.total}")
+
+print("Take book3\n...")
+closet.take(book3.name, book3.author)
+
+print(f"Count unread books: {closet.count_unread}")
+print(f"Count read books: {closet.count_read}")
+print(f"Total count books: {closet.total}")
+
+print("Retrieve book1\n...")
+closet.retrieve(book1.name)
+
+print(f"Count unread books: {closet.count_unread}")
+print(f"Count read books: {closet.count_read}")
+print(f"Total count books: {closet.total}")
+
+print(closet)
 
 print(isinstance(book1, str))
 
--- l.py
+++ l.py
@@ -1,18 +1,49 @@
-while True:
-    try:
-        price = float(input('Enter your price:'))
-        if price >= 0:
-            print('tax = 18%')
-            tax = price * 0.18
-            print(f'tax = {tax}')
-            print('tip = 10%')
-            tip = price * 0.10
-            print(f'tip = {tip}')
-            full_price = price + tax + tip
-            print(f'Full = {full_price}')
-            break
-        else:
-            print('Error')
-    except ValueError:
-        print('Please,enter your price')
+# while True:
+#     try:
+#         price = float(input('Enter your price:'))
+#         if price >= 0:
+#             print('tax = 18%')
+#             tax = price * 0.18
+#             print(f'tax = {tax}')
+#             print('tip = 10%')
+#             tip = price * 0.10
+#             print(f'tip = {tip}')
+#             full_price = price + tax + tip
+#             print(f'Full = {full_price}')
+#             break
+#         else:
+#             print('Error')
+#     except ValueError:
+#         print('Please,enter your price')
+#
+
+# def unique(list1):
+#     unique_list = []
+#     for x in list1:
+#         if x not in unique_list:
+#             unique_list.append(x)
+#     print(unique_list)
+#
+# list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# print("unique")
+# unique(list1)
+#
+# list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# print("unique")
+# unique(list2)
+
+s = "ab,abc,ab,a"
+
+
+def words(text):
+    words = text.split(",")
+    quant = {k: words.count(k) for k in words}
+    maxim = max(quant, key=quant.get)
+    longest = max(words, key=len)
+    return f"{longest} {maxim}"
+
+
+print(words(s))
+
+
 
